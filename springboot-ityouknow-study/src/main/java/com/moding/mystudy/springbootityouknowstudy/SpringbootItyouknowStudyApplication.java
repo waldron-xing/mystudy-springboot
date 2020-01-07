72cdf3ccfbe113eef2ece6f919764ed55db496e3
waldron xing <waldron_xing@163.com>
2020-01-07 09:38:40 +0800

    初始化项目内容分享自页面源码

---
+++ springboot-ityouknow-study/src/main/java/com/moding/mystudy/springbootityouknowstudy/SpringbootItyouknowStudyApplication.java
@@ -0,0 +1,13 @@
+package com.moding.mystudy.springbootityouknowstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootItyouknowStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootItyouknowStudyApplication.class, args);
+	}
+
+}
---
+++ springboot-ityouknow-study/src/test/java/com/moding/mystudy/springbootityouknowstudy/SpringbootItyouknowStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moding.mystudy.springbootityouknowstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootItyouknowStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
